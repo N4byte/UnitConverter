cef85f6ab01a6b5976111c837914cf4da440f97c
JailsonPA <124307911+JailsonPA@users.noreply.github.com>
2024-05-05 21:31:03 +0100

    Add project files.

---
+++ Controllers/TemperatureController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnitConverter.Models;
+
+namespace UnitConverter.Controllers
+{
+    public class TemperatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Convert(string convertType, double temperature)
+        {
+            TemperatureConverter converter = new TemperatureConverter();
+
+            if (convertType == "ToFahrenheit")
+            {
+                converter.Celsius = temperature;
+                ViewBag.Result = $"Fahrenheit (°F): {converter.CelsiusToFahrenheit()} ºF";
+                ViewBag.Calculation = $"Calculation: {temperature} ºC x 9/5 + 32 = {converter.CelsiusToFahrenheit()} ºF";
+            }
+            else if (convertType == "ToCelsius")
+            {
+                converter.Fahrenheit = temperature;
+                ViewBag.Result = $"Celsius (°C): {converter.FahrenheitToCelsius()} ºC";
+                ViewBag.Calculation = $"Calculation: ({temperature} ºF - 32) x 5/9 = {converter.FahrenheitToCelsius()} ºC";
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ Models/TemperatureConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace UnitConverter.Models
+{
+    public class TemperatureConverter
+    {
+        public double? Celsius { get; set; }
+        public double? Fahrenheit { get; set; }
+
+        public double CelsiusToFahrenheit()
+        {
+            if (!Celsius.HasValue)
+            {
+                throw new InvalidOperationException("The Celsius value is zero.");
+            }
+
+            return (Celsius.Value * 9 / 5) + 32;
+        }
+
+        public double FahrenheitToCelsius()
+        {
+            if (!Fahrenheit.HasValue)
+            {
+                throw new InvalidOperationException("The Fahrenheit value is zero.");
+            }
+
+            return (Fahrenheit.Value - 32) * 5 / 9;
+        }
+    }
+}
